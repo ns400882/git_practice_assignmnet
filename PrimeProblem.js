b4f4bfd3159c89c8c066fef8d939e6245e947323
unknown <sanjurathaur99@gmail.com>
2022-12-06 22:32:21 +0530

    problem completed

--- PrimeProblem.js
+++ PrimeProblem.js
@@ -2,13 +2,13 @@ let num=13;
 let count =0;
 for (let i=1;i<=num;i++){
 
-	if (num%i==2){
-                           count++;
+	if (num%i==0){
+            count++;
 	}
 }
-if (count ==1){
-        console.log("Prime")
+if (count ==2){
+        console.log("Prime");
      }
      else{
-         console.log("Not Prime")
+         console.log("Not Prime");
      }
